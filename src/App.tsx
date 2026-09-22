3f6ff6b026535413de079cdcbc693fb86dcbba5e
George Rodopoulos <grodop2004@gmail.com>
2026-09-22 15:52:16 +0100

    Improve caption contrast and screen reader access

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { gallery } from './data/gallery'
 function App() {
   return (
     <main>
+      <h1 className="visually-hidden">Gambling Nature</h1>
       <AnimalGrid animals={gallery} />
     </main>
   )
